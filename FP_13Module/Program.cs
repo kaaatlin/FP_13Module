e5af59a164f31d83b235fee1d2fc8517377624ac
admin <admin@DESKTOP-NKFQO7K>
2022-10-24 18:51:06 +0300

    Добавьте файлы проекта.

---
+++ FP_13Module/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+
+//Наша задача — сравнить производительность вставки в List<T> и LinkedList<T>.Для этого используйте уже знакомый вам StopWatch.
+
+namespace FP_13Module
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\admin\\Desktop\\Text1.txt";
+
+            var timer = Enter_List(path);
+
+            Console.WriteLine("Производительность вставки в List<T>: " + timer.ElapsedMilliseconds.ToString() + " мс");
+
+            var time = Enter_LinkedList(path);
+
+            Console.WriteLine("Производительность вставки в LinkedList<T>: " + time.ElapsedMilliseconds.ToString() + " мс");
+        }
+
+        static Stopwatch Enter_List(string path)
+        {
+            var textList = new List<string>();
+
+            var timer = new Stopwatch();
+            timer.Start();
+            // Вставка в List<T>
+            textList = File.ReadAllLines(path).ToList();
+
+            timer.Stop();
+            return timer;
+        }
+
+        static Stopwatch Enter_LinkedList(string path)
+        {
+            var txtLinkedList = new LinkedList<string>();
+            // Потоки чтения
+            FileStream fs = new FileStream(path, FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+
+            var time = new Stopwatch();
+            time.Start();
+            // Пока не окончится чтение, вставка в LinkedList<T>
+            while (!sr.EndOfStream)
+            {
+                txtLinkedList.AddLast(sr.ReadLine());
+            }
+            sr.Close();
+
+            time.Stop();
+            return time;
+        }
+    }
+}
---
+++ Sec_FP_13Module/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Sec_FP_13Module
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "C:\\Users\\admin\\Desktop\\Text1.txt";
+            string line;
+            using (var reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    ParseLine(line);
+                }
+            }
+            // Сортировка по значениям
+            var items = from pair in Words
+                        orderby pair.Value descending
+                        select pair;
+
+
+            int n = 0;
+            int i = 1;
+            foreach (KeyValuePair<string, int> pair in items)
+            {
+                if (n < 11 && i <= 10)// 10 слов
+                {
+                    Console.WriteLine("{2}: {0} = {1}", pair.Key, pair.Value, i);
+                    n++;
+                    i++;
+                }
+                else break;
+            }
+
+            Console.ReadKey();
+
+        }
+
+        static Dictionary<string, int> Words = new Dictionary<string, int>();
+        static void ParseLine(string s)
+        {
+            string word = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsLetter(s[i]))
+                {
+                    word += s[i];
+                }
+                else
+                {
+                    word = word.ToLower();
+                    if (Words.ContainsKey(word))
+                    {
+                        Words[word]++;
+                    }
+                    else
+                    {
+                        if (word.Length > 2)
+                        {
+                            Words.Add(word, 1);
+                        }
+                    }
+                    word = "";
+                }
+            }
+        }
+    }
+}
